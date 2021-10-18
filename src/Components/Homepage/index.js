ba0c7db5fbccad29d645927650647f7e613536ff
Harsh <harshg@dewsolutions.in>
2021-10-18 16:40:05 +0530

    route-setup

---
+++ src/Components/Homepage/index.js
@@ -0,0 +1,14 @@
+import logo from "./broad-logo.jpg";
+import "./style.css";
+
+function Homepage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default Homepage;
